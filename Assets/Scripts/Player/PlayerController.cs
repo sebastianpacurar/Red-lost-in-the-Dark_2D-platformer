242f8351f90743daf8c2e6d53acffa941d294cc2
sebastianpacurar <pacurar.sebastian90@gmail.com>
2023-02-10 04:11:11 +0200

    improve player controls:  - fix animation conditionals issues  - add physics circle cast for edge (corner) hanging, along with logic  - fix physics related to walls interaction and free interaction (jump is problematic on WallChecker matched with ground layer)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,21 +3,39 @@ using UnityEngine.InputSystem;
 
 namespace Player {
     public class PlayerController : MonoBehaviour {
-        [SerializeField] private Transform groundCheckerPos, wallCheckerPos;
-        [SerializeField] private LayerMask groundLayer;
-        [SerializeField] private float moveSpeed;
-        [SerializeField] private float jumpForce;
-        [SerializeField] private float climbSpeed;
-
         private PlayerAnimState _animState;
         private PlayerControls _controls;
         private Rigidbody2D _rb;
         private Animator _animator;
-
-        private float _moveVal;
-        private bool _isJumpOn;
-        private bool _isClimbOn;
         private static readonly int State = Animator.StringToHash("state");
+
+        [Header("Shape Casts")]
+        [SerializeField] private Transform groundCheckerPos;
+        [SerializeField] private Transform wallCheckerPos;
+        [SerializeField] private Transform wallEdgeChecker;
+        [SerializeField] private LayerMask groundLayer;
+
+        [Space(10)]
+        [Header("Physics Related")]
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float jumpForce;
+        [SerializeField] private float climbSpeed;
+
+        [Space(10)]
+        [Header("For Debugging Purposes")]
+        [SerializeField] private float moveVal;
+        [SerializeField] private bool isJumpPressed;
+
+        [Space(10)]
+        [Header("Free Jump Related")]
+        [SerializeField] private bool isAscending;
+        [SerializeField] private bool isDescending;
+
+        [Space(10)]
+        [Header("Wall Climbing Related")]
+        [SerializeField] private bool isClimbOn;
+        [SerializeField] private bool isSlideOn;
+        [SerializeField] private bool isHangingOnEdge;
 
 
         private void Awake() {
@@ -26,61 +44,110 @@ namespace Player {
             _animator = GetComponent<Animator>();
         }
 
+        private void Update() {
+            if (!IsGrounded() && IsNextToWall() && !isClimbOn && !isHangingOnEdge) {
+                isSlideOn = true;
+            } else {
+                isSlideOn = false;
+            }
+
+            // if player is facing the wall and the key which runs towards the wall is pressed, prevent move val to be non zero
+            if ((moveVal < 0 && transform.rotation.y.Equals(-1) || (moveVal > 0 && transform.rotation.y.Equals(0f))) && IsNextToWall()) {
+                moveVal = 0;
+            }
+
+            if (_rb.velocity.y > 0f && !IsGrounded() && !IsNextToWall()) {
+                isAscending = true;
+            } else {
+                isAscending = false;
+            }
+
+            if (_rb.velocity.y < 0f && !IsGrounded() && !IsNextToWall()) {
+                isDescending = true;
+            } else {
+                isDescending = false;
+            }
+        }
+
         private void FixedUpdate() {
-            if (IsGrounded() && _isJumpOn) {
+            if (IsGrounded() && isJumpPressed) {
                 var velocity = _rb.velocity;
                 velocity = new Vector2(velocity.x, velocity.y + jumpForce);
                 _rb.velocity = velocity;
-                _isJumpOn = false;
+                isJumpPressed = false;
+                isAscending = true;
             }
-            _rb.velocity = new Vector2(_moveVal * moveSpeed, _rb.velocity.y);
+            _rb.velocity = new Vector2(moveVal * moveSpeed, _rb.velocity.y);
 
             PerformWallClimbing();
+            PerformEdgeHanging();
             SlideOffWall();
 
             HandleAnimations();
             FlipHero();
+
+            if (isHangingOnEdge) {
+                _rb.gravityScale = 0f;
+                _rb.velocity = Vector2.zero;
+            } else {
+                _rb.gravityScale = 1f;
+            }
         }
 
         private void PerformWallClimbing() {
-            if (!_isClimbOn) return;
+            if (!isClimbOn || !IsNextToWall() || isHangingOnEdge) return;
             var velocity = _rb.velocity;
-            velocity = new Vector2(velocity.x, velocity.y + climbSpeed);
+            velocity = new Vector2(velocity.x, climbSpeed);
             _rb.velocity = velocity;
         }
 
+        private void PerformEdgeHanging() {
+            if (isClimbOn && !IsWallEdgeActive()) {
+                isClimbOn = false;
+                isHangingOnEdge = true;
+            } else if (!IsNextToWall()) {
+                isHangingOnEdge = false;
+            }
+        }
+
         private void SlideOffWall() {
-            if (_isClimbOn || IsGrounded() || !IsNextToWall()) return;
+            if (isClimbOn || IsGrounded() || !IsNextToWall() || isHangingOnEdge) return;
             var velocity = _rb.velocity;
-            velocity = new Vector2(velocity.x, velocity.y - climbSpeed);
+            velocity = new Vector2(velocity.x, -climbSpeed);
             _rb.velocity = velocity;
         }
 
         private void Move(InputAction.CallbackContext ctx) {
+            if (isClimbOn || isSlideOn || isHangingOnEdge) {
+                moveVal = 0;
+                return;
+            }
+
             switch (ctx.phase) {
                 case InputActionPhase.Started:
                 case InputActionPhase.Performed:
-                    _moveVal = _controls.Player.Move.ReadValue<float>();
+                    moveVal = _controls.Player.Move.ReadValue<float>();
                     break;
                 case InputActionPhase.Canceled:
-                    _moveVal = 0;
+                    moveVal = 0;
                     break;
             }
         }
 
         private void Jump(InputAction.CallbackContext ctx) {
-            _isJumpOn = true;
+            isJumpPressed = IsGrounded();
         }
 
         private void ClimbWall(InputAction.CallbackContext ctx) {
-            if (!IsNextToWall()) return;
             switch (ctx.phase) {
                 case InputActionPhase.Started:
                 case InputActionPhase.Performed:
-                    _isClimbOn = true;
+                    if (IsNextToWall()) {
+                        isClimbOn = true;
+                    }
                     break;
                 case InputActionPhase.Canceled:
-                    _isClimbOn = false;
+                    isClimbOn = false;
                     break;
             }
         }
@@ -95,24 +162,20 @@ namespace Player {
             return val;
         }
 
+        private bool IsWallEdgeActive() {
+            var val = Physics2D.OverlapCircle(wallEdgeChecker.position, 0.05f, groundLayer);
+            return val;
+        }
+
         private void HandleAnimations() {
-            if (_rb.velocity.x is < 0f or > 0f && IsGrounded()) {
-                _animState = PlayerAnimState.Run;
-            } else if (_rb.velocity.y > 0f && !IsGrounded()) {
-                _animState = PlayerAnimState.Jump;
-            } else if (_rb.velocity.y < 0f && !IsGrounded()) {
-                _animState = PlayerAnimState.Fall;
-            } else {
-                _animState = PlayerAnimState.IdleOne;
-            }
+            _animState = PlayerAnimState.IdleOne;
 
-            if (_isClimbOn) {
-                _animState = PlayerAnimState.WallClimb;
-            }
-
-            if (IsNextToWall() && !IsGrounded()) {
-                _animState = PlayerAnimState.WallSlide;
-            }
+            if (moveVal != 0 && IsGrounded() && _rb.velocity.x != 0) _animState = PlayerAnimState.Run;
+            if (isAscending) _animState = PlayerAnimState.Jump;
+            if (isDescending) _animState = PlayerAnimState.Fall;
+            if (isClimbOn) _animState = PlayerAnimState.WallClimb;
+            if (isSlideOn) _animState = PlayerAnimState.WallSlide;
+            if (isHangingOnEdge) _animState = PlayerAnimState.CornerGrab;
 
             _animator.SetInteger(State, (int)_animState);
         }
