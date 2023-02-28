43a182be0e6c324ec9530ff2093f44f48f565450
sebastianpacurar <pacurar.sebastian90@gmail.com>
2023-02-28 03:23:14 +0200

    animate tilemap, add stuff to demolevel:  - animated waterfalls and lavafalls tiles  - copy the env sprites and save them mirrored as a new png  - rethink level to be based on 3 layers in terms of difficulty

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -36,7 +36,7 @@ public class GameManager : MonoBehaviour {
                 playerObj.transform.Find("TutorialCanvas").gameObject.SetActive(true);
                 break;
             case "DemoLevel":
-                playerObj.transform.position = new Vector3(-130f, 5f, 1f);
+                playerObj.transform.position = new Vector3(-230f, 92f, 1f);
                 playerObj.transform.Find("TutorialCanvas").gameObject.SetActive(false);
                 break;
         }
