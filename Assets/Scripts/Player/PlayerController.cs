56665c053c0da1af0183201c722b47488870ac59
sebastianpacurar <pacurar.sebastian90@gmail.com>
2023-04-15 03:30:48 +0300

    fix auto jump and add auto climb; break ascend-descend-fall animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,5 @@
 using Cinemachine;
 using CustomAttributes;
-using Menu;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
@@ -17,21 +16,29 @@ namespace Player {
         [SerializeField] private float jumpForce;
 
         [SerializeField] private Vector2 wallJumpForce;
-        [SerializeField] private float wallJumpDuration;
+        [SerializeField] private Vector2 wallClimbForce;
+        [SerializeField] private float wallClimbDefaultDuration;
+        [SerializeField] private float wallJumpDefaultDuration;
         [SerializeField] private float wallSlidingSpeed;
+        [SerializeField] private float maxAscendSpeed;
 
-        [SerializeField] private bool isJumpPressed;
+        [ReadOnlyProp] [SerializeField] private bool isJumpPressed;
 
         [ReadOnlyProp] [SerializeField] private float xInputVal;
         [ReadOnlyProp] [SerializeField] private float cachedXInputVal;
         [ReadOnlyProp] [SerializeField] private bool isGrounded;
         [ReadOnlyProp] [SerializeField] private bool isWallActive;
-        [ReadOnlyProp] [SerializeField] private bool isWallClimbing;
         [ReadOnlyProp] [SerializeField] private bool isSliding;
         [ReadOnlyProp] [SerializeField] private float wallSlideMaxSpeed;
-        [ReadOnlyProp] [SerializeField] private bool isWallJumpInitiated;
-        [ReadOnlyProp] [SerializeField] private bool isWallJumpInProgress;
         [ReadOnlyProp] [SerializeField] private float wallJumpDirection;
+        [ReadOnlyProp] [SerializeField] private bool isWallJumpInit;
+        [ReadOnlyProp] [SerializeField] private float wallJumpDurationCd;
+        [ReadOnlyProp] [SerializeField] private bool canAutoWallJump;
+        [ReadOnlyProp] [SerializeField] private bool isWallClimbInit;
+        [ReadOnlyProp] [SerializeField] private float wallClimbDurationCd;
+        [ReadOnlyProp] [SerializeField] private float wallClimbCurrDur;
+        [ReadOnlyProp] [SerializeField] private bool canAutoWallClimb;
+        [ReadOnlyProp] [SerializeField] private bool isAscending;
         [ReadOnlyProp] [SerializeField] private bool isFalling;
 
         [ReadOnlyProp] [SerializeField] private bool isAttacking;
@@ -44,6 +51,8 @@ namespace Player {
         private Animator _animator;
         private SpriteRenderer _sr;
         private HandleHpSanity _stats;
+        private RaycastHit2D _hitLeft;
+        private RaycastHit2D _hitRight;
 
         private static readonly int State = Animator.StringToHash("state");
 
@@ -67,9 +76,15 @@ namespace Player {
             isGrounded = Physics2D.OverlapCapsule(groundChecker.position, new Vector2(0.425f, 0.05f), CapsuleDirection2D.Horizontal, 0, groundLayer);
             isWallActive = Physics2D.OverlapCapsule(wallChecker.position, new Vector2(0.05f, 0.425f), CapsuleDirection2D.Vertical, 0, groundLayer);
 
-            if (isGrounded) {
-                isFalling = false;
-            }
+            var pos = transform.position;
+            _hitLeft = Physics2D.Raycast(pos, Vector2.left, 5.1f, groundLayer);
+            _hitRight = Physics2D.Raycast(pos, Vector2.right, 5.1f, groundLayer);
+
+            HandleWallClimbDurationVal();
+
+            // if (isGrounded) {
+            //     isFalling = false;
+            // }
 
             if (isAttacking) {
                 xInputVal = 0f;
@@ -86,7 +101,7 @@ namespace Player {
             }
 
             if (isSliding) {
-                isFalling = false;
+                // isFalling = false;
                 _sr.flipX = true;
             } else if (isAttacking) {
                 _sr.flipX = true;
@@ -99,6 +114,8 @@ namespace Player {
             }
 
             HandleAnimations();
+            HandleWallClimbDurationCd();
+            HandleWallJumpDurationCd();
         }
 
         private void FixedUpdate() {
@@ -106,64 +123,65 @@ namespace Player {
             if (isGrounded && !isAttacking && isJumpPressed) {
                 // if grounded apply simple jump
                 _rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                if (isWallActive) isWallClimbing = true;
             }
 
-            if (isSliding) {
-                if (isJumpPressed) {
-                    Debug.Log("isSliding and isJumpPressed");
-                    isWallClimbing = false;
-                    isWallJumpInitiated = true;
-                    Invoke(nameof(StopWallJump), wallJumpDuration);
+            if (isSliding && isJumpPressed) {
+                if (_hitLeft && _hitRight && !isWallClimbInit) {
+                    isWallClimbInit = true;
+                } else if ((!_hitLeft || !_hitRight) && !isWallJumpInit) {
+                    isWallJumpInit = true;
                 } else {
-                    wallSlideMaxSpeed = isWallClimbing ? float.MaxValue : 0f;
                     _rb.velocity = new Vector2(_rb.velocity.x, Mathf.Clamp(_rb.velocity.y, -wallSlidingSpeed, wallSlideMaxSpeed));
                 }
             }
 
-            if (isWallJumpInitiated) {
-                Debug.Log("isWallJumpInitiated");
-                // set the next condition to true
-                isWallJumpInProgress = true;
+            // NOTE: Treat Wall Jump and Wall Climb differently. 
+            if (isWallJumpInit) {
+                // Wall Jump
+                canAutoWallJump = true;
 
-                // set direction only once, the first time when isWallJumpInitiated is triggered as true
-                if (wallJumpDirection.Equals(0)) wallJumpDirection = -transform.localScale.x;
-
-                // add wall jump force
-                _rb.AddForce(new Vector2(wallJumpForce.x * wallJumpDirection, wallJumpForce.y), ForceMode2D.Impulse);
-            } else if (isWallJumpInProgress) {
-                Debug.Log("isWallJumpInProgress");
-
-                // apply jump force 
-                _rb.velocity = new Vector2(wallJumpForce.x * wallJumpDirection, _rb.velocity.y);
-
-                if (isJumpPressed) {
-                    Debug.Log("IsJumpPressed check");
+                if (wallJumpDirection.Equals(0)) {
+                    wallJumpDirection = -transform.localScale.x;
                 }
 
-                // set the current condition to false, and set direction to 0, when player is in mid air
-                if (_rb.velocity.y <= 9.5f) {
-                    Debug.Log("changeJumpInProgress to false");
-                    isWallJumpInProgress = false;
-                    wallJumpDirection = 0f;
+                if (wallJumpDurationCd < wallJumpDefaultDuration) {
+                    _rb.AddForce(new Vector2(wallJumpForce.x * wallJumpDirection, wallJumpForce.y), ForceMode2D.Impulse);
+                } else {
+                    _rb.velocity = new Vector2(xInputVal * moveSpeed, _rb.velocity.y);
+                }
+            } else if (isWallClimbInit) {
+                // Wall Climb
+                canAutoWallClimb = true;
+
+                if (wallJumpDirection.Equals(0)) {
+                    wallJumpDirection = -transform.localScale.x;
                 }
 
+                _rb.AddForce(new Vector2(wallClimbForce.x * wallJumpDirection, wallClimbForce.y), ForceMode2D.Impulse);
             } else {
                 _rb.velocity = new Vector2(xInputVal * moveSpeed, _rb.velocity.y);
             }
 
-
             // clamp ascend speed to 12f;
-            _rb.velocity = new Vector2(_rb.velocity.x, Mathf.Clamp(_rb.velocity.y, float.MinValue, 12f));
+            _rb.velocity = new Vector2(_rb.velocity.x, Mathf.Clamp(_rb.velocity.y, float.MinValue, maxAscendSpeed));
 
             // reduce Air Time through gravity value
-            if (_rb.velocity.y < 0f && !isGrounded) {
-                _rb.gravityScale = 2f;
-            } else if (_rb.velocity.y > 5f && _rb.velocity.y > 0f) {
-                _rb.gravityScale = 1.75f;
-            } else {
-                _rb.gravityScale = 1f;
+            _rb.gravityScale = _rb.velocity.y switch {
+                < 0f when !isGrounded => 2f,
+                > 5f and > 0f => 1.75f,
+                _ => 1f
+            };
+
+            // prevent player from sliding upwards the wall
+            if (isSliding && _rb.velocity.y > 0f) {
+                _rb.velocity = new Vector2(_rb.velocity.x, 0f);
             }
+
+            var product = Vector2.Dot(_rb.velocity, wallJumpForce);
+
+            isAscending = product > 0f && !isSliding && !isGrounded;
+            isFalling = product < -10f && !isSliding && !isGrounded;
+            Debug.Log(Vector2.Dot(_rb.velocity, wallJumpForce));
 
             // block inputs and set booleans handled by animations to false
             if (isDead) {
@@ -174,6 +192,51 @@ namespace Player {
             }
 
             FlipPlayerScale();
+        }
+
+        private void HandleWallClimbDurationCd() {
+            if (canAutoWallClimb) {
+                wallClimbDurationCd += Time.deltaTime;
+
+                if (wallClimbDurationCd > wallClimbCurrDur) {
+                    isWallClimbInit = false;
+                    canAutoWallClimb = false;
+                    wallClimbDurationCd = 0f;
+                    wallJumpDirection = 0f;
+                }
+            }
+        }
+
+        private void HandleWallJumpDurationCd() {
+            if (canAutoWallJump) {
+                wallJumpDurationCd += Time.deltaTime;
+
+                if (wallJumpDurationCd > wallJumpDefaultDuration * 2) {
+                    isWallJumpInit = false;
+                    canAutoWallJump = false;
+                    wallJumpDurationCd = 0f;
+                    wallJumpDirection = 0f;
+                }
+            }
+        }
+
+
+        // NOTE: Increases/Decreases wallClimbDuration in case there are narrow halls to climb upwards
+        // NOTE: Value is provided based on the distance between the walls
+        private void HandleWallClimbDurationVal() {
+            // in case both walls are detected, make value dynamic, else set to default
+            if (_hitLeft && _hitRight) {
+                var dist = _hitLeft.distance + _hitRight.distance;
+
+                wallClimbCurrDur = dist switch {
+                    < 2 => 0.275f, // 2 tiles distance between walls
+                    > 2f and < 3f => 0.375f, // 3 tiles distance between walls
+                    > 3f and < 5f => 0.525f, // 4 tiles distance between walls
+                    _ => wallClimbDefaultDuration
+                };
+            } else {
+                wallClimbCurrDur = wallClimbDefaultDuration;
+            }
         }
 
         // set the checkpoint on the next triggered torch
@@ -217,44 +280,20 @@ namespace Player {
             }
         }
 
-        // public void HandleJumpLogic() {
-        //     // if (isGrounded && !isAttacking && isJumpPressed) {
-        //     //     // if grounded apply simple jump
-        //     //     _rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-        //     //
-        //     //     // perform slide upwards when jumping when facing a wall close by
-        //     //     if (isWallActive) isWallClimbing = true;
-        //     // } else if (isSliding && isJumpPressed) {
-        //     //     if (isJumpPressed) {
-        //     //         _rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-        //     //
-        //     //         // perform slide upwards when jumping when facing a wall close by
-        //     //         if (isWallActive) isWallClimbing = true;
-        //     //     }
-        //     // } else if (isSliding) {
-        //     //     isWallClimbing = false;
-        //     //     // if sliding start wallJumping process
-        //     //     isWallJumpInitiated = true;
-        //     //     Invoke(nameof(StopWallJump), wallJumpDuration);
-        //     // }
-        // }
-
-        // prevent double wall jump in mid air
-        private void StopWallJump() {
-            isWallJumpInitiated = false;
-        }
-
         private void HandleAnimations() {
             var state = AnimationState.Idle;
 
             if (xInputVal != 0f && isGrounded) state = AnimationState.Run;
-            state = _rb.velocity.y switch {
-                > 0f when !isGrounded => AnimationState.Ascend,
-                < 0f when !isGrounded => AnimationState.Descend,
-                _ => state
-            };
-            if (isFalling) state = AnimationState.Fall;
+
+            // state = _rb.velocity.y switch {
+            //     > 0f when !isGrounded => AnimationState.Ascend,
+            //     < 0f and > -1f when !isGrounded => AnimationState.Descend,
+            //     _ => state
+            // };
+
             if (isSliding) state = AnimationState.Slide;
+            if (isAscending) state = AnimationState.Ascend;
+            if (isFalling) state = AnimationState.Fall;
 
             if (isAttacking) {
                 state = Random.Range(6, 9) switch {
@@ -309,10 +348,11 @@ namespace Player {
             _controls.Player.Jump.Disable();
         }
 
-        // called in Descend Animation as event
-        public void SetIsFallingTrue() {
-            isFalling = true;
-        }
+        //TODO:  broke entire animator because of this
+        // // called in Descend Animation as event
+        // public void SetIsFallingTrue() {
+        //     isFalling = true;
+        // }
 
         // called in every Attack animation as event
         public void StopAttackEvent() {
